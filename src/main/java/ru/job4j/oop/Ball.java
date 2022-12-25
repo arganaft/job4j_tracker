9dd95780807d73b8e9aaff67fa13cb97377401f1
arganaft <e1mar.arganaft@gmail.com>
2022-12-26 00:44:28 +0300

    1.2.1. Классы и объекты / 1.6. Взаимодействие объектов

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        System.out.println(condition ? "колобок съеден :(" : "колобок сбежал :)");
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Fox fox = new Fox();
+        Wolf wolf = new Wolf();
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
