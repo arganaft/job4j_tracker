e5d157c9e140427467e3dcc7fc2de6957c26d5ef
arganaft <e1mar.arganaft@gmail.com>
2022-12-25 23:16:25 +0300

    1.2.1. Классы и объекты / 1.4. Вызов метода с возвращаемым типом.

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,7 +2,6 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String translation = "Неизвестное слово. " + eng;
-        return translation;
+        return "Неизвестное слово. " + eng;
     }
 }
