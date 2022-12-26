0efb7eaf41af71a2e8eb44372fead9e0c0d254ab
arganaft <e1mar.arganaft@gmail.com>
2022-12-27 01:29:39 +0300

    1.2.1. Классы и объекты / 1.7. Конструктор

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Error is active?: " + active);
+        System.out.println("Status number: " + status);
+        System.out.println("Warning message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        defaultError.printInfo();
+        Error blueError = new Error(true, 1, "Blue threat level");
+        blueError.printInfo();
+        Error yellowError = new Error(true, 2, "Yellow threat level");
+        yellowError.printInfo();
+        Error redError = new Error(true, 3, "Red threat level");
+        redError.printInfo();
+    }
+}
