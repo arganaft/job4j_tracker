b7daf06df0991c042017e61745b2c83387303d9b
arganaft <e1mar.arganaft@gmail.com>
2022-12-31 23:38:12 +0300

    1.2.2. Наследование / 2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -21,17 +21,14 @@ public class Item {
     }
 
     public void setId(int id) {
-
         this.id = id;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 }
