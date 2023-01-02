73f76b0d7410ea3713e36f28037afcfee9fba5e3
arganaft <e1mar.arganaft@gmail.com>
2023-01-02 03:39:45 +0300

    1.2.3. Инкапсуляция / 3.1. Массив с пустыми ячейками

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            }
+        }
+    }
+
+    public static int indexOfNull(Product[] products) {
+        int indexOfNull = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                indexOfNull = i;
+                break;
+            }
+        }
+        return indexOfNull;
+    }
+}
