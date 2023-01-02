c8739ac6a6f829965cf3fed3d0bee8be165fcbfd
arganaft <e1mar.arganaft@gmail.com>
2023-01-02 18:55:51 +0300

    1.2.3. Инкапсуляция / 3. Массивы и модели.

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -4,6 +4,13 @@ public class Book {
     private String name;
     private int pages;
 
+    public Book() {
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,11 +2,10 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book cleanCode = new Book();
+        Book cleanCode = new Book("Clean code");
         Book designPatterns = new Book();
         Book thePragmaticProgrammer = new Book();
         Book codeComplete = new Book();
-        cleanCode.setName("Clean code");
         Book[] books = new  Book[4];
         books[0] = cleanCode;
         books[1] = designPatterns;
@@ -22,7 +21,7 @@ public class Library {
             System.out.println(books[index].getName());
         }
         for (int index = 0; index < books.length; index++) {
-            if (books[index].equals(cleanCode)) {
+            if ("Clean code".equals(books[index].getName())) {
                 System.out.println(books[index].getName());
             }
         }
