4d11caf4ac250962c844a81985d703095d876f79
arganaft <e1mar.arganaft@gmail.com>
2023-01-06 17:47:01 +0300

    1.2.4. Полиморфизм / 2.0. Чтение из консоли. Класс Scanner.

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        Scanner input = new Scanner(System.in);
+        String question = input.nextLine();
+        int random = new Random().nextInt(3);
+        String answer = switch (random) {
+            case 0 -> "Да";
+            case 1 -> "Нет";
+            default -> "Может быть";
+        };
+        System.out.println(answer);
+    }
+}
