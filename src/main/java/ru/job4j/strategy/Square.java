a59c537fd12710e88a0615720baee16249019848
arganaft <e1mar.arganaft@gmail.com>
2023-01-13 18:06:45 +0300

    1.2.4. Полиморфизм 11.0. Шаблон проектирования - Стратегия

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,12 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "-------" + ln
+                + "|     |" + ln
+                + "|     |" + ln
+                + "-------" + ln;
+    }
+}
