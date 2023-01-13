a36bbf585a7285f2369a043e36b7d75864f9ff6a
arganaft <e1mar.arganaft@gmail.com>
2023-01-13 19:51:43 +0300

    1.2.4. Полиморфизм / 12.0. Написать тесты на StartUI.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -37,9 +37,7 @@ public class StartUITest {
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Deleted item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
                 new String[] {"0", String.valueOf(item.getId()), "1"}
         );
