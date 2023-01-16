bf237fa4ccb07a979d9f7e19ebae4df9b9ab4607
arganaft <e1mar.arganaft@gmail.com>
2023-01-16 20:15:28 +0300

    1.2.5. Исключения / 0.3. Тестирование исключений с junit

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Fact().calc(-34);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+    }
+}
