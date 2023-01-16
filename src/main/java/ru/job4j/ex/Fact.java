70cd261f7b097e5629bf649c528e3f7cf2e46236
arganaft <e1mar.arganaft@gmail.com>
2023-01-16 20:01:51 +0300

    1.2.5. Исключения / 0.2. Кидаем исключение - throw new RuntimeException

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N could not be less then 0");
+        }
+        int rsl = 1;
+        for (int index = 2; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+        System.out.println(new Fact().calc(-1));
+    }
+}
