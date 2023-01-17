7c15010a1d320e6fc708b24f22dbf93e82fc5c64
arganaft <e1mar.arganaft@gmail.com>
2023-01-17 15:30:18 +0300

    1.2.5. Исключения / 0.3. Тестирование исключений с junit

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -16,4 +16,11 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
     }
+
+    @Test
+    public void whenNotException() {
+        int expected = 24;
+        int result = new Fact().calc(4);
+        assertThat(result).isEqualTo(expected);
+    }
 }
