0885ecdb6a6fdedd0b412cb180829a9fcd0d221f
arganaft <e1mar.arganaft@gmail.com>
2023-01-19 22:10:30 +0300

    1.1. Тесты на StartUI.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -192,4 +192,26 @@ public class StartUITest {
                         + "=== Exit Program ===" + System.lineSeparator()
         );
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+        );
+    }
 }
