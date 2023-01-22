f49777b13d1bb4dce6fbadfc5df27a50d88765ff
arganaft <e1mar.arganaft@gmail.com>
2023-01-22 17:22:30 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,17 +1,30 @@
 package ru.job4j.tracker;
 
 public class ValidateInput extends ConsoleInput {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
+            String rsl = in.askStr(question);
             try {
-                value = super.askInt(question);
+                value = Integer.parseInt(rsl);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again.");
+                System.out.println("Please enter validate data again");
             }
         } while (invalid);
         return value;
