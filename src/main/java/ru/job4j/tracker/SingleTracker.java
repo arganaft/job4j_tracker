39754d7f8376752e585439f1e68532863176e6c5
arganaft <e1mar.arganaft@gmail.com>
2023-01-28 03:03:44 +0300

    1. Singleton

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+
+    private static Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
