2775a4a5ce543cae0b7fc63b52c0cb12f2b33e61
arganaft <e1mar.arganaft@gmail.com>
2023-02-19 01:15:38 +0300

    1. Интерфейс Queue

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,26 @@
+package ru.job4j.queue;
+
+import java.util.Objects;
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int i = 0; i < this.count - 1; i++) {
+            queue.poll();
+        }
+        return Objects.requireNonNull(queue.poll()).name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        return Objects.requireNonNull(queue.poll()).name();
+    }
+}
