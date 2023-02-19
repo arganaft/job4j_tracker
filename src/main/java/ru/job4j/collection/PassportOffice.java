f27d1c152c417b6fcd2869f67a88fc1097209779
arganaft <e1mar.arganaft@gmail.com>
2023-02-19 15:24:44 +0300

    1. Паспорт и жители

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (citizens.get(citizen.getPassport()) == null) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+}
