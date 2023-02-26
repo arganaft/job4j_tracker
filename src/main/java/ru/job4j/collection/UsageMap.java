a30751166f2217d083aea0213659e6defea7eaa3
arganaft <e1mar.arganaft@gmail.com>
2023-02-26 22:22:05 +0300

    0. Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,11 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("ElIsaeva@mail.ru", "Елена Исаева");
+        map.put("ElIsaev@mail.ru", "Эльдар Исаев");
+        map.put("ElIsaev@mail.ru", "Эльмар Исаев");
         map.put("k5t3hi@mail.ru", "Сергей Дмитриев");
+        map.put("k5t3347@mail.ru", "Сергей Дмитриев");
+        map.put("PArsetyev@mail.ru", "Павел Арсентьев");
         map.put("PArsetyev@mail.ru", "Пётр Арсентьев");
         for (String key : map.keySet()) {
             String value = map.get(key);
