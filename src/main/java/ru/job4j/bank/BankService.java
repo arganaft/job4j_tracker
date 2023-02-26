47b3e731dbb076afc15becaab21fd6918b47eff4
arganaft <e1mar.arganaft@gmail.com>
2023-02-26 23:43:34 +0300

    3. Банковские переводы

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,18 +9,18 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<>());
-        }
+        users.putIfAbsent(user, new ArrayList<>());
+
     }
 
     public boolean deleteUser(String passport) {
-        return users.remove(findByPassport(passport)) != null;
+        return users.remove(new User(passport, "")) != null;
     }
 
     public void addAccount(String passport, Account account) {
-        if (!users.get(findByPassport(passport)).contains(account)) {
-            users.get(findByPassport(passport)).add(account);
+        List<Account> userAccounts = users.get(findByPassport(passport));
+        if (!userAccounts.contains(account)) {
+            userAccounts.add(account);
         }
 
     }
@@ -30,6 +30,7 @@ public class BankService {
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
                 findUser = user;
+                break;
             }
         }
         return findUser;
@@ -37,10 +38,12 @@ public class BankService {
 
     public Account findByRequisite(String passport, String requisite) {
         Account findAccount = null;
-        if (findByPassport(passport) != null) {
-            for (Account account : users.get(findByPassport(passport))) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
                 if (account.getRequisite().equals(requisite)) {
                     findAccount = account;
+                    break;
                 }
             }
         }
@@ -50,15 +53,9 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        User srsUser = findByPassport(srcPassport);
-        User destUser = findByPassport(destPassport);
         Account srsAccount = findByRequisite(srcPassport, srcRequisite);
         Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srsUser != null
-           && destUser != null
-           && srsAccount != null
-           && destAccount != null
-           && srsAccount.getBalance() >= amount) {
+        if (srsAccount != null && destAccount != null && srsAccount.getBalance() >= amount) {
             srsAccount.setBalance(srsAccount.getBalance() - amount);
             destAccount.setBalance(destAccount.getBalance() + amount);
             rsl = true;
