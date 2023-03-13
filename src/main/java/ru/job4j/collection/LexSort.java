6f5accc4696832661f648f9c3861aab7429fe47f
arganaft <e1mar.arganaft@gmail.com>
2023-03-13 05:57:33 +0300

    4. Сортировка номера

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,7 +6,6 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int result =  0;
         String[] numberLeft = left.split("\\.");
         String[] numberRight = right.split("\\.");
         return Integer.compare(Integer.parseInt(numberLeft[0]), Integer.parseInt(numberRight[0]));
