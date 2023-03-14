97c179d963c5dab0f3d972c5b1da5347e32e1eac
arganaft <e1mar.arganaft@gmail.com>
2023-03-14 13:47:10 +0300

    3. Отсортировать департаменты

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,11 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int res = o1.split("/")[0].compareTo(o2.split("/")[0]);
-        if (res != 0) {
-            return res * -1;
-        } else {
-            return o1.compareTo(o2);
-        }
+        int res = o2.split("/")[0].compareTo(o1.split("/")[0]);
+        return res != 0 ? res : o1.compareTo(o2);
     }
 }
