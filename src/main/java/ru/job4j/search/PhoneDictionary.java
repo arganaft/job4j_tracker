ed1dc3176be9e4c12be96452a336a9a39e4f1f10
arganaft <e1mar.arganaft@gmail.com>
2023-03-16 23:57:55 +0300

    7. Функции высшего порядка

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -16,11 +17,12 @@ public class PhoneDictionary {
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> combine = person -> person.getPhone().contains(key)
+                || person.getAddress().contains(key)
+                || person.getName().contains(key)
+                || person.getSurname().contains(key);
         for (Person person : persons) {
-            if (person.getPhone().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getName().contains(key)
-                    || person.getSurname().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
